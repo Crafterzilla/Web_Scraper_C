7c0f725708fb42e8489aae927ee66ee99494b2f8
Osmar Rojas <crafterzilla2.1@gmail.com>
2025-03-14 15:28:02 -0400

    Add files for the different roles to be editted

---
+++ include/web_scraper.h
@@ -0,0 +1,35 @@
+#ifndef WEB_SCRAPER_H
+#define WEB_SCRAPER_H
+
+#include <stdio.h>
+#include <curl/curl.h>
+//Add includes go here
+
+
+// Enum to represent if an error occured
+enum CURL_CODE {
+    SUCCESS,
+    WEBSITE_NOT_FOUND,
+    ERROR
+    //ADD MORE CODES FOR MORE ERRORS
+};
+
+
+/*
+Desc: The basic single threaded web_scraper. This function
+takes a website and checks and sees if the website is active 
+or exists. If an error occurs, it returns a code representing
+the error. If success, then it prints out the HTML file to the 
+./output/"filename paramter".html
+
+Input:
+    filename: Name of file for output file
+    website: website to be scraped and see HTML contents
+Ret: An error code or success code
+
+*/
+enum CURL_CODE web_scraper(const char* filename, const char* website);
+
+
+
+#endif
---
+++ src/count.c
@@ -0,0 +1,40 @@
+#include "../include/count.h"
+
+
+/*
+Desc: Takes in an html file and counts the number of time
+the word occurs.
+
+Input:
+    html_data: Html file to be read from
+    word: Special word to count
+Ret: Returns the number of times word occured
+*/
+int count_reoccurance(const FILE html_data, const char* word) {
+    //Code goes here
+}
+
+/*
+Desc: Takes in an html file and counts all the words'
+reoccurance. The array of strings will be in sorted 
+order from a to z. It will give back the reoccurances
+as an int array stored on the heap.
+
+Ex: 
+    Input: ["cat", "dog", "lizard"]
+
+    Algorithm finds 5 cats, 2 dogs, and 0 lizards
+
+    Return: [5, 2, 0]
+
+Input:
+    html_data: Html file to be read from
+    words: Sorted Array of words that will be counted
+    size: size of array words
+Ret: Returns the an array of the number of times those words occured
+in the order they came in.
+
+*/
+int* count_all_reoccurances(const FILE html_data, const char** words, const int size) {
+    //Code goes here
+}
