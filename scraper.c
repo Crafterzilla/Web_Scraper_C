8ff72b4bb7538919839c08a7c23e730aae685781
Osmar Rojas <crafterzilla2.1@gmail.com>
2025-03-14 15:24:52 -0400

    Edit documentation. Add roles. Change scraper filename

---
+++ scraper.c
@@ -0,0 +1,7 @@
+#include "include/web_scraper.h"
+#include "include/multithread.h"
+#include "include/count.h"
+
+int main() {
+    // Osmar will put all your code together here
+}
--- web_scraper.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "include/test.h"
-
-int main() {
-    test_print("A test string to print\n");
-}
